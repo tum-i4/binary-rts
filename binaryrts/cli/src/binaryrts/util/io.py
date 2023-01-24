9039d859e3f6fe2c7c27a58454c7efe4e78a9bc3
Daniel Elsner <daniel.elsner@tum.de>
2023-01-24 16:55:22 +0100

    [cli] Fix utf-8 encoding issues in IO utils

--- binaryrts/cli/src/binaryrts/util/io.py
+++ binaryrts/cli/src/binaryrts/util/io.py
@@ -6,7 +6,7 @@ from typing import List, Tuple
 def slice_file_into_chunks(file: Path, line_ranges: List[Tuple[int, int]]) -> List[str]:
     ranges: List[range] = [range(min(r), max(r) + 1) for r in line_ranges]
     chunks: List[str] = ["" for _ in ranges]
-    with file.open(mode="r") as fp:
+    with file.open(mode="r", encoding="utf-8") as fp:
         max_line_no: int = max([(min(r), max(r)) for r in line_ranges], key=itemgetter(1))[1]
         for line_no, line in enumerate(fp):
             for chunk_idx, line_range in enumerate(ranges):
